da0959a7bf13330247906b7d83dc0e74f781266b
HashiCorp Training <training@hashicorp.com>
2017-09-18 14:38:55 +0000

    Additional changes

--- main.tf
+++ main.tf
@@ -18,12 +18,22 @@
 #     terraform-training-duck
 #
 
+terraform {
+  backend "atlas" {
+    name = "marojor/training"
+  }
+}
+
 variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
 
 variable "aws_region" {
   default = "eu-central-1"
+}
+
+variable "num_web_instances" {
+  default = "2"
 }
 
 provider "aws" {
@@ -33,6 +43,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
+  count                  = "${var.num_web_instances}"
   ami                    = "ami-958128fa"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-6987b713"
@@ -41,6 +52,20 @@ resource "aws_instance" "web" {
   tags = {
     Identity    = "terraform-training-duck"
     Creator     = "marojor"
-    Description = "Training"
+    Description = "Terraform training for the people"
+    Name        = "web ${count.index + 1}/${var.num_web_instances}"
   }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  # Square brackets are optional
+  value = "${aws_instance.web.*.public_dns}"
+}
+
+output "name" {
+  value = "${aws_instance.web.*.tags.Name}"
 }
